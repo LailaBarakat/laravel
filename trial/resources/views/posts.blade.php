1cf6324da2433fdbb7f975f4b5705bac5b85c9f9
Laila Barakat <laila.n.barakat@gmail.com>
2021-04-26 17:11:09 +0200

    laravel installed, html added with css, already tried js

---
+++ trial/resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<title>Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+<article>
+
+    <h1><a href="/post"> My 1st post </a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consectetur cupiditate debitis eaque ex facilis illum minima provident similique soluta! Autem dolorem iure laboriosam officia quasi vel. Itaque, iure, laboriosam.
+    </p>
+
+</article>
+
+<article>
+
+    <h1><a href="/post"> My 2nd post </a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consectetur cupiditate debitis eaque ex facilis illum minima provident similique soluta! Autem dolorem iure laboriosam officia quasi vel. Itaque, iure, laboriosam.
+    </p>
+
+</article>
+
+<article>
+
+    <h1><a href="/post"> My 3rd post </a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consectetur cupiditate debitis eaque ex facilis illum minima provident similique soluta! Autem dolorem iure laboriosam officia quasi vel. Itaque, iure, laboriosam.
+    </p>
+
+</article>
+
+</body>
